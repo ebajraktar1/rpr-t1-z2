11316364d20021a6496fb5147bf3437adcbc0e7b
Emir Bajraktarević <ebajraktar1@etf.unsa.ba>
2021-10-18 18:00:33 +0200

    Prvi

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara (int n)
+    {
+        int suma=0;
+        while (n>0) {
+            suma = suma + (n % 10);
+            n = n / 10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+	int n=0;
+    Scanner in = new Scanner(System.in);
+    System.out.println("Unesite broj n: ");
+    n=in.nextInt();
+    for (int i=1;i<n;i++)
+        if (i%sumaCifara(i)==0)
+            System.out.println(i);
+    }
+}
